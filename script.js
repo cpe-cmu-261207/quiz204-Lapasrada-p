6d052f83e4b4adf9808a0d41577e2dc81d71ec60
ploylapas <lapasrada_k@cmu.ac.th>
2021-07-29 14:00:48 +0700

    test

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-const author = document.querySelector("#author")
-const calculation = document.querySelector("#calculation")
+const author = document.querySelector("#author");
+const calculation = document.querySelector("#calculation");
 const btn_toggle = document.querySelector("#toggle");
-const length = document.getElementById('length')
-const color = document.querySelector('#color')
+const length = document.getElementById('length');
+const color = document.querySelector('#color');
 
 // define more constants and variables here
+const btn_search = document.querySelector("#search");
+const btn_reset = document.querySelector("#reset");
+const text = document.querySelector("#text");
 
 btn_toggle.onclick = () => {
-  // your code here
+  if(btn_toggle.innerText=="Display Calculation"){
+      btn_toggle.innerText = "Display Author";
+      let code = 630610757;
+      let plus = length.value;
+      let sum = code-plus;
+      author.innerText = (sum).toString();
+  }else{
+      btn_toggle.innerText = "Display Calculation";
+      author.innerText = "630610757 LAPASRADA KUMJULLA";
+  }
 }
 
 // more codes for Search and Reset buttons here
+btn_reset.onclick = () => {
+  length.value =5;
+} 
+
+btn_search.onclick = () => {
+
+}
+
+function searchWord() {
+
+
+}
+
